a2b68d9bd8062557667786d96a5026acb55ac588
Katheryn Guasca <Katheryng2017@gmail.com>
2024-11-17 20:21:44 -0500

    Agregando modificaciones a Manejo de Excepciones

--- tallerNotasAOP/src/main/java/edu/javeriana/tallernotasAOP/aspecto/ManejoExcepcionesAspecto.java
+++ tallerNotasAOP/src/main/java/edu/javeriana/tallernotasAOP/aspecto/ManejoExcepcionesAspecto.java
@@ -14,22 +14,40 @@ import java.util.List;
 @Component
 public class ManejoExcepcionesAspecto {
 
-    // Este método intercepta cualquier excepción lanzada por los métodos de los controladores
+    /**
+     * Este método intercepta cualquier excepción lanzada por los métodos de los controladores
+     * y devuelve un error genérico @param ex La excepción lanzada
+     * @return ResponseEntity con el mensaje de error
+     */
     @AfterThrowing(pointcut = "execution(* edu.javeriana.tallernotasAOP.controlador.*.*(..))", throwing = "ex")
     public ResponseEntity<RespuestaError> manejarExcepciones(Exception ex) {
+        // Creación de la lista de detalles de la excepción
         List<String> detalles = new ArrayList<>();
         detalles.add(ex.getLocalizedMessage());
 
+        // Creación de la respuesta de error
         RespuestaError error = new RespuestaError("Error Interno del Servidor", detalles);
+
+        // Devolver el ResponseEntity con el código HTTP 500 (Error Interno del Servidor)
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    // Método específico para manejar excepciones de tipo RuntimeException (cuando no se encuentra un registro)
+    /**
+     * Método específico para manejar excepciones de tipo RuntimeException,
+     * que usualmente indican que no se encontró un registro.
+     * @param ex La excepción lanzada
+     * @return ResponseEntity con el mensaje de error para registros no encontrados
+     */
     @AfterThrowing(pointcut = "execution(* edu.javeriana.tallernotasAOP.controlador.*.get*(..))", throwing = "ex")
     public ResponseEntity<RespuestaError> manejarRegistroNoEncontrado(RuntimeException ex) {
+        // Creación de la lista de detalles de la excepción
         List<String> detalles = new ArrayList<>();
         detalles.add(ex.getLocalizedMessage());
+
+        // Creación de la respuesta de error para registros no encontrados
         RespuestaError error = new RespuestaError("Registro No Encontrado", detalles);
+
+        // Devolver el ResponseEntity con el código HTTP 404 (No Encontrado)
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
